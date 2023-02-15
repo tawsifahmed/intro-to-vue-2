02f0991260b83c5ad1ebc6e41bda5790c0ba0f9c
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2023-02-15 13:32:26 +0600

    l12, looping with -v-for

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 new Vue({
     el: '#vue-app',
     data: {
-        error: false,
-        success: false,
-        available1: false,
-        nearby1: false,
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ],
     },
     methods: {
 
 
     },
     computed: {
-        compClasses: function () {
-            return {
-                available: this.available1,
-                nearby: this.nearby1
-            }
-        }
+
     }
 });      
